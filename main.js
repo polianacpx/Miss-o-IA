0e710a52f1654e7c539b4980e26067911f4bf789
polianacpx <104083440+polianacpx@users.noreply.github.com>
2024-06-11 08:16:29 -0300

    projeto

---
+++ main.js
@@ -0,0 +1,33 @@
+ const caixaPrincipal =.querySelector(".caixa-principal");
+ const caixaPerguntas =.querySelector(".caixa-perguntas");
+ const caixaAlternativas =.querySelector(".caixa-alternativas");
+ const caixaResultado =.querySelector(".caixa-resultado");
+ const textoResultado =.querySelector(".texto-resultado");
+ 
+ const perguntas = [
+    {
+        enunciado: "Pergunta 1"
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+        
+      },
+      { 
+        enunciado: "Pergunta 3",
+        alterntivas:[
+           "Alternativa 1",
+           "Alternativa 2"
+        ]
+
+      }
+     
+ ];
+  
